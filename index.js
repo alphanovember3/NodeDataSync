e6ebdf3a410dc357e44dadad8d994f7b342daab4
user <ayushnalawade2233@gmail.com>
2025-01-08 16:22:17 +0530

    Redis connection and operations added

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const restify = require('restify');
 const server = restify.createServer();
-const connection = require('./connection');
-const connectionsql1 = require('./sqlconnection');
+const connection = require('./mongoConnection');
+const connectionsql1 = require('./sqlConnection');
 const mysql = require('mysql2/promise');
-server.use(restify.plugins.bodyParser());
 const { v4: uuidv4 } = require('uuid');
-// const { createClient } = require('redis'); 
 const Redis = require("ioredis");
-
-// const redis = require('redis');
+server.use(restify.plugins.bodyParser());
+const { Client } = require('@elastic/elasticsearch')
 
 
 //database connection mongoDB
@@ -16,8 +14,8 @@ let db;
 connection().then(async (database) => {
     db = database;
     //starting server after DB connection 
-    server.listen(process.env.PORT,process.env.IP_PORT, () => {
-    console.log("server running on port 8080");
+    server.listen(process.env.PORT, process.env.IP_PORT, () => {
+        console.log("server running on port 8080");
     });
 });
 
@@ -71,7 +69,7 @@ server.del('/mysql/delete/:id', async (req, res) => {
 // Create a user and save it in MONGO
 server.post('/mongo/create', async (req, res) => {
     const user = {
-        id:uuidv4(),
+        id: uuidv4(),
         name: req.body.name,
         email: req.body.email
     };
@@ -83,56 +81,122 @@ server.post('/mongo/create', async (req, res) => {
 });
 
 //update the user in MONGO
-server.put('/mongo/update/:id', async(req,res)=>{
+server.put('/mongo/update/:id', async (req, res) => {
     const id = req.params.id;
-    const user={
-        name:req.body.name,
-        email:req.body.email
+    const user = {
+        name: req.body.name,
+        email: req.body.email
     }
     const usercollection = db.collection("users");
-    await usercollection.updateOne({ id: id },{ $set: user });
+    await usercollection.updateOne({ id: id }, { $set: user });
     res.send("User Updated")
     return;
 })
 
 //delete the user in mongoDB
-server.del('/mongo/delete/:id',async (req,res)=>{
+server.del('/mongo/delete/:id', async (req, res) => {
     const id = req.params.id;
     const usercollection = db.collection("users");
-    await usercollection.deleteOne({id:id});
+    await usercollection.deleteOne({ id: id });
     res.send("User deleted");
     return;
 })
 
-//Show user all users in mongoDB
-server.get('/mongo/get',async (req,res)=>{
+//Show all users in mongoDB
+server.get('/mongo/get', async (req, res) => {
     const usercollection = db.collection("users");
     const allusers = await usercollection.find().toArray();
     res.send(allusers);
     return;
 })
 
+//Redis Routes
 //Redis connection 
+const redis = new Redis({ port: process.env.REDIS_PORT, host: process.env.REDIS_IP });
+//Create the new user
+server.post('/redis/create/:hashkey', async (req, res) => {
 
-const redis = new Redis({port:process.env.REDIS_PORT,host:process.env.REDIS_IP});
-
-// const client = createClient({ url:'192.168.0.94:6379'});
-
-server.post('/redis/create', async (req,res)=>{
-
-    
+    let hashkey = req.params.hashkey;
     await redis.hset(
-        'userayush2',{
-            'name': req.body.name,
+        hashkey, {
+        'name': req.body.name,
         'email': req.body.email
-        }
+    }
     )
-    res.send("succss")
+    return res.send("data submitted to Redis")
+});
+
+//update the existing user data
+server.put('/redis/update/:hashkey', async (req, res) => {
+
+    let hashkey = req.params.hashkey;
+    await redis.hset(
+        hashkey, {
+        'name': req.body.name,
+        'email': req.body.email
+    }
+    )
+    return res.send("data updated to Redis")
+});
+
+//to show users from redis
+server.get('/redis/get/:hashkey', async (req, res) => {
+    //hashkey name = userAyush1
+    let hashkey = req.params.hashkey;
+    const response = await redis.hgetall(hashkey);
+    return res.send(response);
+});
+
+//to delete user data from redis
+server.del('/redis/delete/:hashkey', async (req, res) => {
+    let hashkey = req.params.hashkey;
+    await redis.del(hashkey);
+    return res.send("successfully Deleted data from Redis");
+});
+
+//Elastic Search Routes
+//elastic connection
+const client = new Client({
+    node: 'http://192.168.0.128:9200/'
 })
 
-//to get from redis
-server.get('/redis/get',async (req,res)=>{
-    const response = await redis.hgetall('userayush');
+//To create data into elastic
+server.post('/elastic/create/', async (req, res) => {
+    await client.index({
+        index: req.body.index,
+        id: req.body.id,
+        document: {
+            name: req.body.name,
+            lastname: req.body.lastname,
+        },
+    });
+    return res.send("data submitted to Elastic")
+});
 
-    res.send(response);
+//To show data from elasticSearch 
+server.get('/elastic/get/:index', async (req, res) => {
+    const index = req.params.index;
+    const response = await client.search({ index: index });
+    return res.send(response);
+})
+
+//update the elastic data 
+server.put('/elastic/update/:index/:id', async (req, res) => {
+    const index = req.params.index;
+    const id = req.params.id;
+    const data = req.body;
+    const result = await client.update({
+        index: index,
+        id: id,
+        doc: data,
+    });
+    res.json(result);
+})
+
+//delete the data from elastic
+server.del('/elastic/delete/:index/:id', async (req, res) => {
+    const index = req.params.index;
+    const id = req.params.id;
+    await client.delete({ index: index, id: id });
+    res.send("data deleted successfully");
 })
